82080f5bf7add0eef3b5714c01d965af09109f7d
lukas enoch chengo <lukas.enoch.chengo29@gmail.com>
2025-11-04 21:45:44 +0300

    sumbua

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+function increment(n){
+    n++;
+    return n;
+}
+function decrement(n){
+    n--;
+    return n;
+}
+function makeInt(string){
+    parseInt("string", 10);
+    return string;
+}
+function preserveDecimal(string){
+    parseFloat("string");
+    return string;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+   return parseFloat(n);
+}
